75125a2f5cdafda85171b643743e823677505478
hamaguchi-tamago <hamaguchi.amago@gmail.com>
2019-05-25 14:59:00 +0900

    random bomb add

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
+import random
+
+def create_cell():
+	BOMB_NUMBER = 10
+	cell =  [['' for i in range(9)] for j in range(9)]
+	
+	# 爆弾をランダムに配置
+	index = 0
+	while index <= BOMB_NUMBER:
+		row = random.randint(0, 8)
+		col = random.randint(0, 8)
+		value = cell[row][col]
+		if	value != 'B':
+			cell[row][col] = 'B'
+			index += 1
+	return cell
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	cell = [["1", "2", "3", "4", "5", "6", "7", "8",""],
-			["", "B", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],
-			["", "", "", "", "", "", "", "", ""],]
+	cell = create_cell()
 	return render_template("index.html", cell = cell)
 
 if __name__ == "__main__":
